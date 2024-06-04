1bb203b5da2955eb0bd90dc579c04cb09025952d
22jdillon <22jdillon@wakatipu.school.nz>
2024-06-05 11:32:59 +1200

    added_basic_menu

--- main.py
+++ main.py
@@ -7,24 +7,40 @@ cursor = conn.cursor() #This connects to the cursur (the flashing line) in your 
 def get_all_guns():
     cursor.execute('SELECT * FROM Rainbow_6_Guns;') #This tells the cursor to type/execute 'SELECT * FROM Rainbow_6_Guns;'
 
-    guns = cursor.fetchall() #this gets the output of the last statment
+    all_guns = cursor.fetchall() #this gets the output of the last statment
 
-    for gun in guns:
-        print(f"Name: {gun[1]:<16}  Weapon Type: {gun[2]:<20}  Damage: {gun[3]:<5}  Fire rate: {gun[4]:<6}  Mobility: {gun[5]:<5}  Capacity: {gun[6]:<7}")
+    for gun in all_guns:
+        print(f"Name: {gun[1]:<14} Weapon Type: {gun[2]:<17} Damage: {gun[3]:<3} Fire rate: {gun[4]:<4} Mobility: {gun[5]:<1} Capacity: {gun[6]:<1}")
 
 def get_guns_by_damage():
 
     cursor.execute('SELECT name, weapon_type, damage FROM Rainbow_6_Guns ORDER BY damage DESC;')
 
-    guns = cursor.fetchall()
+    high_damage = cursor.fetchall()
 
-    for gun in guns:
+    for gun in high_damage:
         print(f"Name: {gun[0]:<16}  Weapon Type: {gun[1]:<17}  Damage: {gun[2]:<5}")
 
 def get_guns_by_fire_rate():
+
     cursor.execute('SELECT name, weapon_type, fire_rate FROM Rainbow_6_Guns WHERE fire_rate > 0 ORDER BY fire_rate DESC;')
 
-    guns = cursor.fetchall()
+    high_fire_rate = cursor.fetchall()
 
-    for gun in guns:
+    for gun in high_fire_rate:
         print(f"Name: {gun[0]:<16}  Weapon Type: {gun[1]:<17}  Fire rate: {gun[2]:<5}")
+
+while True:
+    choice = input("\n1 = Get all\n2 = Get damage \n3 = Get fire rate\n4 = Exit\n> ")
+    if choice == "1":
+        get_all_guns()
+    elif choice == "2":
+        get_guns_by_damage()
+    elif choice == "3":
+        get_guns_by_fire_rate()
+    elif choice == "4" :
+        break
+    else:
+        print("\nInvalid input!")
+
+
