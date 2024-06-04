b99ca62a9f7bb66277e8bc77530ca7560bcb9216
22jdillon <22jdillon@wakatipu.school.nz>
2024-06-05 09:54:44 +1200

    defined_all_views
    dont have a menu to print them yet

--- main.py
+++ main.py
@@ -4,23 +4,30 @@ conn = sqlite3.connect('R6-Guns.db') #This connects to the 'R6-Guns.db' table
 
 cursor = conn.cursor() #This connects to the cursur (the flashing line) in your database
 
-cursor.execute('SELECT * FROM Rainbow_6_Guns;') #This tells the cursor to type/execute 'SELECT * FROM Rainbow_6_Guns;'
 
-guns = cursor.fetchall() #this gets the output of the last statment
 
-for gun in guns:
-    print(f"Name: {gun[1]:<16}  Weapon Type: {gun[2]:<20}  Damage: {gun[3]:<5}  Fire rate: {gun[4]:<6}  Mobility: {gun[5]:<5}  Capacity: {gun[6]:<7}")
 
-cursor.execute('SELECT name, weapon_type, damage FROM Rainbow_6_Guns ORDER BY damage DESC;')
+def get_all_guns():
+    cursor.execute('SELECT * FROM Rainbow_6_Guns;') #This tells the cursor to type/execute 'SELECT * FROM Rainbow_6_Guns;'
 
-guns = cursor.fetchall()
+    guns = cursor.fetchall() #this gets the output of the last statment
 
-for gun in guns:
-    print(f"Name: {gun[0]:<16}  Weapon Type: {gun[1]:<17}  Damage: {gun[2]:<5}")
+    for gun in guns:
+        print(f"Name: {gun[1]:<16}  Weapon Type: {gun[2]:<20}  Damage: {gun[3]:<5}  Fire rate: {gun[4]:<6}  Mobility: {gun[5]:<5}  Capacity: {gun[6]:<7}")
 
-cursor.execute('SELECT name, weapon_type, fire_rate FROM Rainbow_6_Guns WHERE fire_rate > 0 ORDER BY fire_rate DESC;')
+def get_guns_by_damage():
 
-guns = cursor.fetchall()
+    cursor.execute('SELECT name, weapon_type, damage FROM Rainbow_6_Guns ORDER BY damage DESC;')
 
-for gun in guns:
-    print(f"Name: {gun[0]:<16}  Weapon Type: {gun[1]:<17}  Fire rate: {gun[2]:<5}")
+    guns = cursor.fetchall()
+
+    for gun in guns:
+        print(f"Name: {gun[0]:<16}  Weapon Type: {gun[1]:<17}  Damage: {gun[2]:<5}")
+
+def get_guns_by_fire_rate():
+    cursor.execute('SELECT name, weapon_type, fire_rate FROM Rainbow_6_Guns WHERE fire_rate > 0 ORDER BY fire_rate DESC;')
+
+    guns = cursor.fetchall()
+
+    for gun in guns:
+        print(f"Name: {gun[0]:<16}  Weapon Type: {gun[1]:<17}  Fire rate: {gun[2]:<5}")
