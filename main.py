e09d98800188d8c41aa0a5d07851769ca4d1af62
22jdillon <22jdillon@wakatipu.school.nz>
2024-06-04 15:09:49 +1200

    Added a new "view"
    damage (High to Low) and sorted the output

--- main.py
+++ main.py
@@ -10,3 +10,10 @@ guns = cursor.fetchall() #this gets the output of the last statment
 
 for gun in guns:
     print(f"Name: {gun[1]:<16}  Weapon Type: {gun[2]:<20}  Damage: {gun[3]:<5}  Fire rate: {gun[4]:<6}  Mobility: {gun[5]:<5}  Capacity: {gun[6]:<7}")
+
+cursor.execute('SELECT name, weapon_type, damage FROM Rainbow_6_Guns ORDER BY damage DESC;')
+
+guns = cursor.fetchall()
+
+for gun in guns:
+    print(f"Name: {gun[0]:<16}  Weapon Type: {gun[1]:<17}  Damage: {gun[2]:<5}")
